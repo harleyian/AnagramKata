64573618294c061b10ca734b2616bace39ca2dcd
Ian Harley <ian.harley@asos.com>
2018-05-25 10:19:59 +0100

    Test for empty dictionary

---
+++ Anagrams/Anagrams.UnitTests/AngramGeneratorShould.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Anagrams.UnitTests
+{
+    [TestClass]
+    public class AngramGeneratorShould
+    {
+        [TestMethod]
+        public void HandlesEmptyList()
+        {
+            var anagramGenerator = new AnagramGenerator();
+            anagramGenerator.AddDictionary(null);
+
+            var anagramList = anagramGenerator.GetAnagrams();
+            Assert.IsNotNull(anagramList);
+            Assert.Equals(anagramList.Count, 0);
+        }
+    }
+}
---
+++ Anagrams/Anagrams.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Anagrams.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Anagrams.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("08f6ee17-4430-48b1-a9f1-857d4218872d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
